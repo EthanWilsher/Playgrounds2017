5963b9b52be1d8186dc3a0a667de3f0604250a68
wilshdog <31513793+wilshdog@users.noreply.github.com>
2017-09-25 08:36:46 -0600

    Played with the variable types.

--- IntroToSwift.playground/Contents.swift
+++ IntroToSwift.playground/Contents.swift
@@ -1,5 +1,31 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+//Explicit type decleratioon with no value
+var ramen :String
+//Implicit Type decleration with a value to assign type.
+var soup = "Ramen"
+ramen = soup
+print(ramen)
+print(soup)
+//ramen = 7 (does not work)
+var myAge :Int
+myAge = 16
+print(myAge)
+myAge = myAge * 3
+var someValue = 3.1415 * 56
+someValue = someValue * 231.1
+var isReal :Bool
+isReal = false
+isReal = true
+let myName :String
+myName = "Ethan"
+print(myName)
+//myName = soup
+//Can't assign into a let once assigned
+var cake :String
+var pastry = "Cake"
+cake = pastry
+print(pastry)
+print(cake)
 
-var str = "Hello, playground"
